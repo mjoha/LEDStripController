04d51435474cff5346ec20d6493c44648aa451a7
mjoha <mathias.a.johansson@gmail.com>
2017-06-07 18:36:30 +0200

    Adding new classes to start clean up of dirty solution.s

---
+++ LEDStripController/AudioTracker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LEDStripController
+{
+    class AudioTracker
+    {
+        public struct Microphone
+        {
+        }
+
+        public struct SpeakerOutput
+        {
+
+        }
+        /*MMDeviceEnumerator devEnum = new MMDeviceEnumerator();
+        MMDevice defaultDevice = devEnum.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Multimedia);
+        float currVolume = defaultDevice.AudioMeterInformation.MasterPeakValue;
+                return (int) (currVolume* 100);*/
+    }
+}
